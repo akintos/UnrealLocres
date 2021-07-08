ffbd3c09ae9f4b9bd07d0e23697b1bfd449242cd
akintos <akintos47@gmail.com>
2021-07-08 21:45:13 +0900

    Set project author

--- LocresLib/LocresString.cs
+++ LocresLib/LocresString.cs
@@ -8,23 +8,17 @@ namespace LocresLib
     {
         public string Key { get; set; }
         public string Value { get; set; }
-        public uint SourceStringHash { get; }
+
+        /// <summary>
+        /// <see cref="Crc.StrCrc32"/> hash of source string.
+        /// </summary>
+        public uint SourceStringHash { get; } 
 
         public LocresString(string key, string value, uint sourceStringHash)
         {
             Key = key ?? throw new ArgumentNullException(nameof(key));
             Value = value ?? throw new ArgumentNullException(nameof(value));
             SourceStringHash = sourceStringHash;
-        }
-
-        public static implicit operator string(LocresString l)
-        {
-            return l.Value;
-        }
-
-        public override string ToString()
-        {
-            return Value;
         }
     }
 }
